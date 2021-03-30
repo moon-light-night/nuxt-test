fa47c37a2faab55dfbe4ebd9313f6286b446d066
Ilya Baskakov <baskakov.11111@mail.ru>
2021-03-30 10:46:06 +0300

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Nuxt SSR</a>
+
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <nuxt-link exact active-class="active" class="nav-link" to="/"
+              >> Home
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link active-class="active" class="nav-link" to="/about"
+              >> About
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h2>About page</h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias,
+      reiciendis.
+    </p>
+    <hr />
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+      aspernatur!
+    </p>
+    <hr />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nobis!
+    </p>
+  </section>
+</template>
